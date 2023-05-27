35f509949767a705269fb7e2b35c95b97c04891c
Zoe Guidry <oyezoey@gmail.com>
2023-05-26 21:07:45 -0500

    lab completed.

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,9 @@
 
 class Dog:
     # Class body goes here
-
     #Instance method definition
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
     pass
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,9 @@
 
 class Person:
     # Class body goes here
-
     #Instance method definition
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
     pass
